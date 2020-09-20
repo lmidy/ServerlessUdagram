6721ebdcc1df009f1f22cd9b3a5ceab82bfec30e
lmidy <lynette.midy@gmail.com>
2020-09-20 10:57:24 -0500

    forgot to push cors changes to git

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -15,6 +15,10 @@ export const handler = middy(
     logger.info('get todos', event)
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
       body: JSON.stringify({
         items: todos
       })
